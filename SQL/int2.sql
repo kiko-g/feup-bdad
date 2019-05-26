c39ec6c6fb95c546575a05ee045bf312a166ce1f
LuisPRamos <49035388+LuisPRamos@users.noreply.github.com>
2019-05-26 17:46:54 +0100

    final

--- SQL/int2.sql
+++ SQL/int2.sql
@@ -2,14 +2,19 @@
 .headers	on
 .nullvalue	NULL
 
--- 2 Todas as encomendas feitas em Abril contendo Amaciador
+-- 2 Produto mais vendido do mês passado
 
-CREATE VIEW encomendasAbrilComAmaciador
+CREATE VIEW IF NOT EXISTS EncomendasMesPassado AS
+SELECT idEncomenda FROM Encomenda
+WHERE strftime('%Y-%m', data) = strftime('%Y-%m', 'now', '-1 month');
 
-SELECT * FROM Encomenda
-  WHERE Encomenda.idEncomenda IN (SELECT Encomenda.idEncomenda FROM
-  QuantidadePedida, Encomenda, Produto
-  WHERE QuantidadePedida.idEncomenda = Encomenda.idEncomenda
-  AND QuantidadePedida.codigoBarras = Produto.codigoBarras
-  AND Produto.nome = 'Amaciador'
-  AND Encomenda.data LIKE '2019-04%');
+CREATE VIEW IF NOT EXISTS numeroComprasProduto AS
+SELECT codigoBarras, SUM(quantidade) AS 'quantidade'
+FROM EncomendasMesPassado INNER JOIN QuantidadePedida ON 
+  EncomendasMesPassado.idEncomenda = QuantidadePedida.idEncomenda
+GROUP BY codigoBarras;
+
+
+SELECT A.codigoBarras AS 'Codigo de Barras', nome AS 'Nome', marca AS 'Marca', preço AS 'Preço', desconto AS 'Desconto'
+  FROM (Produto INNER JOIN (SELECT numeroComprasProduto.codigoBarras, MAX('quantidade') FROM numeroComprasProduto) AS maxProduto 
+                          ON maxProduto.codigoBarras = Produto.codigoBarras) AS A; 
