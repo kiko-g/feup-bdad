f2f8e62a007e725dbe086a591987533bb56fb329
LuisPRamos <49035388+LuisPRamos@users.noreply.github.com>
2019-05-26 20:12:21 +0100

    gatilho 1 adiciona

--- SQL/gatilho1_adiciona.sql
+++ SQL/gatilho1_adiciona.sql
@@ -8,24 +8,16 @@
 
 PRAGMA foreign_keys = ON;
 
-CREATE TRIGGER preco_Encomenda
+CREATE TRIGGER insertEncomenda
 AFTER INSERT ON Encomenda
 FOR EACH ROW
-BEGIN
-CASE
 WHEN(NEW.idTransportadora IS NOT NULL)
+BEGIN
   UPDATE Encomenda
     SET preçoFinal = (SELECT preço FROM Transportadora 
                         WHERE Transportadora.idTransportadora = NEW.idTransportadora)
-  WHERE NEW.idEncomenda = Encomenda.idEncomenda
-
-AFTER INSERT ON Encomenda
-FOR EACH ROW
-BEGIN
-CASE
-WHEN(NEW.idTransportadora IS NOT NULL)
-    INSERT INTO Entrega 
-    VALUES (NEW.idTransportadora, NEW.idEncomenda, SELECT morada from cliente WHERE cliente.nif = NEW.nif)
+  WHERE NEW.idEncomenda = Encomenda.idEncomenda;
+  INSERT INTO Entrega(idTrasportadora, idEncomenda, morada)
+              VALUES (NEW.idTransportadora, NEW.idEncomenda, (SELECT morada from Pessoa WHERE Pessoa.NIF = NEW.NIF));
 END;
-
     
