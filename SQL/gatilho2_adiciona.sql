96d1988284ea69cb5b47c8bd5784d55d9dcaa229
LuisPRamos <49035388+LuisPRamos@users.noreply.github.com>
2019-05-26 20:03:32 +0100

    gatilhos 1 e 2 feitos

--- SQL/gatilho2_adiciona.sql
+++ SQL/gatilho2_adiciona.sql
@@ -6,28 +6,14 @@ PRAGMA foreign_keys = ON;
 
 -- Incrementa o preço final da encomenda de acordo com a quantidade pedida e decrementa o stock da loja
 
-CREATE IF NOT EXISTS TRIGGER preçoInsertQuantidade
+CREATE TRIGGER insertQuantidade
 AFTER INSERT ON QuantidadePedida
 FOR EACH ROW
-BEGIN
-  SELECT 
-  CASE 
-  WHEN(NEW.quantidade > 0)
-    UPDATE Encomenda
-      SET preçoFinal += NEW.quantidade * (SELECT preço FROM Produto 
-                          WHERE Produto.codigoBarras = NEW.codigoBarras) * (1 - (SELECT desconto FROM Produto 
-                          WHERE Produto.codigoBarras = NEW.codigoBarras) / 100)
-    WHERE NEW.idEncomenda = Encomenda.idEncomenda
-    
-    UPDATE Stock
-      SET stock -= NEW.quantidade
-      WHEN(stock < 0) RAISE(ROLLBACK, 'Quantidade indisponível')
-    WHERE NEW.codigoBarras = Stock.codigoBarras
-  END;
+WHEN(NEW.quantidade > 0) BEGIN
+    UPDATE Encomenda SET preçoFinal = preçoFinal + NEW.quantidade * (SELECT preço FROM Produto WHERE Produto.codigoBarras = NEW.codigoBarras) * 
+                                            (1 - (SELECT desconto FROM Produto WHERE Produto.codigoBarras = NEW.codigoBarras) / 100)
+    WHERE NEW.idEncomenda = Encomenda.idEncomenda;
 
-  SELECT
-  CASE
-  WHEN(NEW.quantidade <= 0)
-    RAISE(ROLLBACK, 'Quantidade inválida')
-  END;
+    UPDATE Stock SET stock = stock - NEW.quantidade
+    WHERE NEW.codigoBarras = Stock.codigoBarras;
 END;
