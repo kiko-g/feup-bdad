d79ed04320326eb8b42d33404bb608f30524218b
LuisPRamos <49035388+LuisPRamos@users.noreply.github.com>
2019-05-26 17:46:12 +0100

    nomes

--- SQL/int6.sql
+++ SQL/int6.sql
@@ -5,6 +5,6 @@
 -- 6
 -- Preco final medio das encomendas do cliente anterior
 
-SELECT A.NIF, A.email, avg(preçoFinal) FROM
+SELECT A.NIF, A.email, avg(preçoFinal) AS 'Preço Final médio' FROM
 (Cliente INNER JOIN Encomenda ON Cliente.NIF = Encomenda.NIF)
 AS A GROUP BY A.NIF;
